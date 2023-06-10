f90ac6f6e31ecf417bcfafa8844aab3b7d2bb541
Suraj8707 <114924118+Suraj8707@users.noreply.github.com>
2023-06-10 12:33:59 +0530

    #10

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,7 +8,8 @@ const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <SubHeading title="When something feels off, it is." />
-      <h1 className="app__header-h1">Fine Design With Fine Work</h1>
+      <h1 className="app__header-h1">"Excellence in design"</h1>
+      <h1 className="app__header-h1">"Perfection in execution"</h1>
       <p className="p__opensans" style={{ margin: '2rem 0' }}>Our company "SWASTIK ENTERPRISES" specializes in interior work and maintenance services. We pride ourselves on providing high-quality workmanship and exceptional customer service. Our team of skilled professionals is dedicated to ensuring that every project is completed to the highest standards. Whether you need help with a small repair or a complete renovation, we have the expertise and experience to get the job done right.</p>
     </div>
 
